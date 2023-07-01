b854664d78c93d30a48908e6022765abfa4ba0ef
Александр <81959083+pitbrest@users.noreply.github.com>
2023-07-01 18:19:09 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <nav className="teal">
       <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">React Shop</a>
+        <a href="/" className="brand-logo">React Shop</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><a href="https://github.com/pitbrest/react-shop" target="_blank"
             rel="noreferrer">Repo</a></li>
